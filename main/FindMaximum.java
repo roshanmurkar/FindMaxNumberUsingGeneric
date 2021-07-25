405254f35dd02b75feb035ce85b8aae28eac1fb8
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-07-25 20:39:10 +0530

    refactorTwo

--- main/FindMaximum.java
+++ main/FindMaximum.java
@@ -13,7 +13,7 @@ public class FindMaximum {
 		Float float3 = 3.3f;
 		
 		String string = "Apple";
-		String string2 = "mango";
+		String string2 = "Mango";
 		String string3 = "Banana";
 
 		
@@ -24,6 +24,15 @@ public class FindMaximum {
 		refactor2.findMax(float1, float2, float3);
 		
 		RefactorOne refactor3=new RefactorOne<Integer>();
-		refactor3.findMax(string, string2, string3);	
+		refactor3.findMax(string, string2, string3);
+		
+		RefactorTwo<Integer> refactor22=new RefactorTwo<Integer>(1,2,3);
+		refactor22.findMax(1, 2, 3);
+		
+		RefactorTwo<Float> refactor23=new RefactorTwo<Float>(1.1f,2.2f,3.3f);
+		refactor23.findMax(1.1f,2.2f,3.3f);
+		
+		RefactorTwo<String> refactor24=new RefactorTwo<String>("apple","mango","banana");
+		refactor24.findMax("apple","mango","banana");
 	}
 }
