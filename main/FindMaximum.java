88ef3e8dccea101ca6d443802b93e8e35a1e748e
unknown <roshanmurkar7168@gmail.comgit config --global user.name roshanmurkar>
2021-07-25 20:35:10 +0530

    refectorTwo created

--- main/FindMaximum.java
+++ main/FindMaximum.java
@@ -1,6 +1,6 @@
 package main;
 import main.RefactorOne;
-import main.RefactorTwo;
+
 
 public class FindMaximum {
 	public static void main(String[] args) {
@@ -8,18 +8,15 @@ public class FindMaximum {
 		Integer integer2 = 70;
 		Integer integer3 = 74;
 
-
 		Float float1 = 1.1f;
 		Float float2 = 2.2f;
 		Float float3 = 3.3f;
-
-
+		
 		String string = "Apple";
 		String string2 = "mango";
 		String string3 = "Banana";
 
 		
-		//generating generic method object
 		RefactorOne refactor1=new RefactorOne<Integer>();
 		refactor1.findMax(integer1, integer2, integer3);
 		
@@ -27,16 +24,6 @@ public class FindMaximum {
 		refactor2.findMax(float1, float2, float3);
 		
 		RefactorOne refactor3=new RefactorOne<Integer>();
-		refactor3.findMax(string, string2, string3);
-		
-		//generting generic class object
-		RefactorTwo<Integer> refactor22=new RefactorTwo<Integer>(1,2,3);
-		refactor22.findMax(1, 2, 3);
-		
-		RefactorTwo<Float> refactor23=new RefactorTwo<Float>(1.1f,2.2f,3.3f);
-		refactor23.findMax(1.1f,2.2f,3.3f);
-		
-		RefactorTwo<String> refactor24=new RefactorTwo<String>("apple","mango","peach");
-		refactor24.findMax("apple","mango","peach");
+		refactor3.findMax(string, string2, string3);	
 	}
 }
---
+++ main/Uc2.java
@@ -0,0 +1,22 @@
+package main;
+
+public class Uc2 {
+	public static Float findMax(Float firstNumber, Float secondNumber, Float thirdNumber) {
+		Float value = null;
+		if (thirdNumber.compareTo(firstNumber)>0 && thirdNumber.compareTo(secondNumber)>0) {
+			System.out.println(thirdNumber+" third is greater");
+			value=thirdNumber;
+			
+		}
+		if (firstNumber.compareTo(secondNumber)>0 && firstNumber.compareTo(thirdNumber)>0) {
+			System.out.println(firstNumber+" first is greater ");
+			value=firstNumber;
+		}
+		if (secondNumber.compareTo(firstNumber)>0 && secondNumber.compareTo(thirdNumber)>0) {
+			System.out.println(secondNumber+" second is greater");
+			value=secondNumber;
+		}
+		return value;
+	}
+
+}
